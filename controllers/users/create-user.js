4bd120c0bbf1e8d295f5fa73e87de7727ee7e244
jeannot diambu <diambujeannot@gmail.com>
2023-11-03 10:05:55 +0100

    find one user

--- controllers/users/create-user.js
+++ controllers/users/create-user.js
@@ -1,20 +1,33 @@
-const cryptPassword = require("../../services/crypt-password");
+const cryptPassword = require("../../services/bcrypt/crypt-password");
 
 const User = require("../../models").User;
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
 
+  console.log("email", email);
+
+  const findUser = await User.findOne({
+    where: { email, name },
+  });
+
   try {
-    const user = await User.create({
-      name,
-      email,
-      password: cryptPassword(password),
-    });
-    if (user) {
-      res.status(200).json({ message: "success", user });
+    if (!findUser) {
+      const user = await User.create({
+        name,
+        email,
+        password: cryptPassword(password),
+      });
+      if (user) {
+        res.status(200).json({ message: "success", user });
+      } else {
+        res.status(500).json({
+          message:
+            "Le serveur a rencontré une erreur et n'a pas pu traiter votre demande.",
+        });
+      }
     } else {
-      res.status(500).json({ message: "User is not created" });
+      res.status(500).json({ message: "Cet utilisateur existe déjà. Veillez modifier vos informations." });
     }
   } catch (err) {
     res.status(400).json(err);
