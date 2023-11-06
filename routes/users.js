852b1b797bd6464bfe4044f6e9f9d9993b020aef
jeannot diambu <diambujeannot@gmail.com>
2023-11-06 12:29:22 +0100

    compare password user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 const { createUser } = require('../controllers/users/create-user');
 const { getUsers } = require('../controllers/users/get-users');
+const login = require('../controllers/auth/login');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/postUser', createUser);
 router.get('/getUsers', getUsers);
+router.post('/auth/login', login);
 
 module.exports = router;
