cac61e777b775ad527ed082c916193031ba7fe76
jeannot diambu <diambujeannot@gmail.com>
2023-11-09 12:15:10 +0100

    create role

--- controllers/roles/create-role.js
+++ controllers/roles/create-role.js
@@ -1,5 +1,32 @@
+const Role = require("../../models").Role;
+// const User = require("../../models").User;
+
 const createRole = async (req, res) => {
-  console.log("Create role");
+  const { name } = req.body;
+
+  const checkUser = await Role.findOne({ name });
+
+  try {
+    if (!checkUser) {
+      const role = await Role.create({ name });
+      if (role) {
+        res.status(200).json({ message: "success", role });
+      } else {
+        res.status(500).json({
+          message:
+            "Le serveur a rencontré une erreur et n'a pas pu traiter votre demande.",
+          data: null,
+        });
+      }
+    } else {
+      res.status(500).json({
+        message: "Cet role existe deja",
+        data: null,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = createRole;
