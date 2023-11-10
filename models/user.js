7850cb3de7867fcc8bbba0637a934ac9e4effdd4
jeannot diambu <diambujeannot@gmail.com>
2023-11-10 17:01:44 +0100

    add foregn keys in user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const Role = require("./position");
+const Site = require("./site");
+const Contact = require("./contact");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,15 +12,46 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(models.Role);
+      User.belongsTo(models.Site);
+      User.belongsTo(models.Contact);
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+
+      //Associations
+      RoleId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Role,
+          key: "id",
+        },
+      },
+
+      SiteId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Site,
+          key: "id",
+        },
+      },
+
+      ContactId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Contact,
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
