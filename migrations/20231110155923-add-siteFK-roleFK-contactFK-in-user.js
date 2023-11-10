c25e126ca511f433da8ea0f6d25e90ab25e5bd10
jeannot diambu <diambujeannot@gmail.com>
2023-11-10 17:21:33 +0100

    migrate user

--- migrations/20231110155923-add-siteFK-roleFK-contactFK-in-user.js
+++ migrations/20231110155923-add-siteFK-roleFK-contactFK-in-user.js
@@ -1,22 +1,49 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", "roleId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Roles", // name of Source model
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+
+    await queryInterface.addColumn(
+      "Users", // name of Target model
+      "siteId", // name of the key we're adding
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Sites", // name of Source model
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      }
+    );
+
+    await queryInterface.addColumn("Users", "contactId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Contacts", // name of Source model
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
