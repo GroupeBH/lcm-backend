f529ffb0957e225c814adaf74613f5f387cd8cfd
jeannot diambu <diambujeannot@gmail.com>
2023-11-14 13:20:44 +0100

    create client and add it in sorty

--- models/sorty.js
+++ models/sorty.js
@@ -5,6 +5,7 @@ const {
 const Product = require('./product');
 const Site = require('./site');
 const User = require('./user');
+const Client = require('./client');
 module.exports = (sequelize, DataTypes) => {
   class Sorty extends Model {
     /**
@@ -17,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       Sorty.belongsTo(models.Product);
       Sorty.belongsTo(models.Site);
       Sorty.belongsTo(models.User);
+      Sorty.belongsTo(models.Client);
       
     }
   }
@@ -47,6 +49,13 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       references: {
         model: User,
+        key: 'id'
+      },
+    },
+    UserId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Client,
         key: 'id'
       },
     }
