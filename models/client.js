72296ae1da4bceb7627e9d380f72ccb446fa0812
jeannot diambu <diambujeannot@gmail.com>
2023-11-14 12:28:21 +0100

    model clients

---
+++ models/client.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Client extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Client.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    middlename: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    adressAdress: DataTypes.STRING,
+    aceinAdress: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Client',
+  });
+  return Client;
+};
