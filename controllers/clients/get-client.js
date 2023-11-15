c39c9b36fbf4c2ad32f60d136289a7807fa983cd
jeannot diambu <diambujeannot@gmail.com>
2023-11-15 10:53:05 +0100

    get client

--- controllers/clients/get-client.js
+++ controllers/clients/get-client.js
@@ -8,8 +8,11 @@ const getClient = async (req, res) => {
     if (client) {
       res.status(200).json({ message: "success", client });
     }
+    else{
+      res.status(500).json({ message: "Cet utilisateur n'existe.", });
+    }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 };
 module.exports = getClient;
