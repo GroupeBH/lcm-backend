24e3e7a309aecf71c5962cd755028bf8f7504554
eugene <bosukuege@gmail.com>
2023-11-15 10:12:37 +0100

    add client and user in get-sorty contriller

--- controllers/sorties/get-sorty.js
+++ controllers/sorties/get-sorty.js
@@ -1,12 +1,14 @@
 const Sorty = require("../../models").Sorty;
 const Product = require("../../models").Product;
 const User = require("../../models").User;
+const Client = require("../../models").Client;
+const Contact = require("../../models").Contact;
 
 const getSorty = async (req, res) => {
   console.log(req.params)
   const sorty = await Sorty.findOne({
     where: {id: req.params.id},
-    include: [{model: Product},{model: User}],
+    include: [{model: Product},{model: User, include:[Contact]},{model: Client}],
   });
   console.log("la sortie : ", sorty);
   try {
